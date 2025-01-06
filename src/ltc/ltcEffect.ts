e96605f0067fd3d89ca76173cfa6eba769f6d234
Sergio Ricardo Zerbetto Masson <sergioze@microsoft.com>
2025-01-06 14:58:02 -0300

    Added code to save ltc to binary file

--- src/ltc/ltcEffect.ts
+++ src/ltc/ltcEffect.ts
@@ -9,26 +9,69 @@ import "@babylonjs/core/Shaders/ShadersInclude/importanceSampling";
 import "@babylonjs/core/Maths/math.vector";
 import Module from "./LTCGenerator";
 import { Nullable } from "@babylonjs/core/types";
+import { ToHalfFloat } from "@babylonjs/core/Misc/textureTools";
+import { Tools } from "@babylonjs/core/Misc/tools";
 
 export class LTCEffect {
-
-    constructor(private N: number, private Nsample: number, private MIN_ALPHA: number) {
-    }
+    constructor(
+        private N: number,
+        private Nsample: number,
+        private MIN_ALPHA: number
+    ) {}
 
     public render(): Nullable<Float32Array> {
-        if(Module) {
-            const float32Array = Module.BuildLTC(this.N, this.Nsample, this.MIN_ALPHA);
+        if (Module) {
+            const float32Array = Module.BuildLTC(
+                this.N,
+                this.Nsample,
+                this.MIN_ALPHA
+            );
             const numElements = float32Array.length;
             const ptr = float32Array.byteOffset;
-            const array = new Float32Array(Module.HEAPF32.buffer, ptr, numElements);
+            const array = new Float32Array(
+                Module.HEAPF32.buffer,
+                ptr,
+                numElements
+            );
             return array;
         }
 
         return null;
     }
 
-    public save(data: Float32Array) : void {
+    public save(data: Float32Array): void {
+        const ltc1 = new Float32Array(data.buffer, 0, this.N * this.N * 4);
+        const ltc2 = new Float32Array(data.buffer, ltc1.length, this.N * this.N * 4);
+        
+        // Array with 6 channels per pixel.
+        const result = new Uint16Array(this.N * this.N * 8);
 
+        for (let pixelIndex = 0; pixelIndex < this.N * this.N; pixelIndex++) {
+            // LTC1 R value
+            result[pixelIndex * 8] = ToHalfFloat(ltc1[pixelIndex * 4]);
+
+            // LTC1 G value
+            result[pixelIndex * 8 + 1] = ToHalfFloat(ltc1[pixelIndex * 4 + 1]);
+
+            // LTC1 B value
+            result[pixelIndex * 8 + 2] = ToHalfFloat(ltc1[pixelIndex * 4 + 2]);
+
+            // LTC1 A value
+            result[pixelIndex * 8 + 3] = ToHalfFloat(ltc1[pixelIndex * 4 + 3]);
+
+            // LTC2 R value
+            result[pixelIndex * 8 + 4] = ToHalfFloat(ltc2[pixelIndex * 4]);
+
+            // LTC2 G value
+            result[pixelIndex * 8 + 5] = ToHalfFloat(ltc2[pixelIndex * 4 + 1]);
+
+            // LTC2 B value
+            result[pixelIndex * 8 + 6] = ToHalfFloat(ltc2[pixelIndex * 4 + 2]);
+
+            // LTC2 A value
+            result[pixelIndex * 8 + 7] = ToHalfFloat(ltc2[pixelIndex * 4 + 3]);
+        }
+
+        Tools.DownloadBlob(new Blob([result.buffer]), "ltc.bin");
     }
 }
-
